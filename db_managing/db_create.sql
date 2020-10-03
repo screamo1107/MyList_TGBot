f2fab8965e11dd69a07bade9aed4159152d85ea5
screamo1107 <screamo1107@gmail.com>
2020-10-03 20:07:09 +0200

    Draft schema of Todo_list table

---
+++ db_managing/db_create.sql
@@ -0,0 +1,6 @@
+# DB schema:
+# id,
+# message_id (text) - message.message_id,
+# text (text) - message.text  -command name,
+# priority(enum: P0, P1, P2, P3)  - default P2,
+# deprecated (true/false) - default False,
